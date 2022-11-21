43b7cd8b0b7cafe589405309acc5a11f224cc243
mr-icon <emmanuelaustin116@gmail.com>
2022-11-20 21:10:50 -0800

    pushing back update

--- meme-app/src/App.js
+++ meme-app/src/App.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import Joke from './components/Joke'
+import jokesData from './components/jokesData'
 import './App.css';
 
 function App() {
+      const jokeElements = jokesData.map(joke => {
+            return <Joke setup={joke.setup} punchline={joke.punchline} />
+      })
   return (
     <div className="App">
-      <Joke punchline="It's hard to explain puns to kleptomanics because they always take things literally."
-            isPun={true}
-      />
-      <Joke setup="I got my daughter a fridge for her birthday." 
-            punchline="I can't wait to see her face light up when she opens it."
-            isPun={true}
-      />
-      <Joke setup="How did the hacker escape the police?" 
-            punchline="He just ransomare."
-            isPun={true}
-      />
-      <Joke setup="Why don't prirate travel on mountain roads?" 
-            punchline="Scurvy"
-            isPun={true}
-      />
-      <Joke setup="Why do bees stay in the hive in the winter?" 
-            punchline="swarm"
-            isPun={true}
-      />
-      <Joke setup="What's the best thing about Switerland?" 
-            punchline="Well i don't know but the flag is a big plus!"
-            isPun={false}
-      />
+      {jokeElements}
     </div>
   );
 }
