5b4144f853ffffacc6a349100da6206e668b0252
mr-icon <emmanuelaustin116@gmail.com>
2022-12-08 10:54:56 -0800

    pushing back update

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,19 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import jokesData from "./jokesData";
 
 
 function Form() {
-    const [memeImage, setMemeImage] = React.useState("")
+    const [memeImage, setMemeImage] = useState("")
     function getImage() {
-        const meme = jokesData.data.memes[1]
-        setMemeImage(meme.url)
+        const meme = jokesData.data.memes
+        const randomNumber = Math.floor(Math.random() * meme.length)
+        setMemeImage(meme[randomNumber].url)
+       
     }
+
     return(
         <main>
             <div className="form">
                 <input type="text" placeholder="Top Text" className="form-input" />
                 <input type="text" placeholder="Down Text" className="form-input"/>
-                <button onclick={getImage} className="form-button">Get a new meme image</button>
+                <button onClick={getImage} className="form-button">Get a new meme image</button>
             </div>
             <img src={memeImage} className="meme-image" />
         </main>
